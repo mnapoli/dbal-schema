8db9dfca14d100694ae89d222dd1a0f67ecc2b07
David de Boer <david@ddeboer.nl>
2022-01-22 16:49:06 +0100

    Compare SQL only

--- tests/MigrationTest.php
+++ tests/MigrationTest.php
@@ -6,15 +6,18 @@ use DbalSchema\DbalSchemaProvider;
 use DbalSchema\SchemaDefinition;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 use Doctrine\Migrations\Configuration\Connection\ExistingConnection;
 use Doctrine\Migrations\Configuration\Migration\ConfigurationArray;
 use Doctrine\Migrations\DependencyFactory;
 use Doctrine\Migrations\Provider\SchemaProvider;
+use Doctrine\Migrations\Version\Version;
 use PHPUnit\Framework\TestCase;
 
 class MigrationTest extends TestCase
 {
     private DependencyFactory $migration;
+    private const MIGRATION_NAME = 'DbalSchema\Test\Fixture\Version1';
 
     public function setUp(): void
     {
@@ -44,8 +47,29 @@ class MigrationTest extends TestCase
         $this->migration->setService(SchemaProvider::class, new DbalSchemaProvider($schema));
 
         // 2: generate the migration
-        $file = $this->migration->getDiffGenerator()->generate('DbalSchema\Test\Fixture\V1', null);
+        $this->migration->getDiffGenerator()->generate(self::MIGRATION_NAME, null);
 
-        self::assertFileEquals(__DIR__ . '/Expected/V1.php', $file);
+        // 3. assert
+        $migration = $this->getMigration();
+        $migration->up(new Schema());
+        $this->assertEquals(
+            'CREATE TABLE test (id INTEGER NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id))',
+            $migration->getSql()[0]->getStatement()
+        );
+
+        $migration = $this->getMigration();
+        $migration->down(new Schema());
+        $this->assertEquals(
+            'DROP TABLE test',
+            $migration->getSql()[0]->getStatement()
+        );
+    }
+
+    private function getMigration(): AbstractMigration
+    {
+        // clone is needed to reset state between the up() and down() assertions
+        return clone $this->migration->getMigrationRepository()->getMigration(
+            new Version(self::MIGRATION_NAME)
+        )->getMigration();
     }
 }
